24ed6e687861f4ac739cfd20c418078dfed12c41
LukasKalenda <kalenda.lu.2020@skola.ssps.cz>
2022-11-19 21:25:31 +0100

    Pred upravou

--- pozemky.js
+++ pozemky.js
@@ -16,3 +16,63 @@ for(let i = 0; i < amount; i++) {
 }
 
 console.log(total);
+
+      // console.log("Tada s pridanim trojky: " + data[j + 3]);
+
+      //Tady to chce podminku, kde rovnou zapisu souradnice prvků a pak budu počítat abstraktně
+
+/*
+//navazuje na "total hodnotu" z predchoziho cyklu
+
+let cisloPole = Number(velikostPole[0]);
+console.log("cislo pole na number: " + cisloPole);
+
+let poleCilu = function(sirka, vyska) {
+  let cilovePole = [];
+
+  for (let j = 0; j < sirka; j++) {
+    let row = [];
+
+    for (let k = 0; k < vyska; k++) {
+      row.push(data[j + 2 + cisloPole].charAt(k));
+    }
+    cilovePole.push(row);
+  }
+  return cilovePole;
+}
+
+console.log("Pole cílů: ");
+console.log(poleCilu(velikostPole[0], velikostPole[1]));
+
+*/
+
+
+
+
+
+// let herniPole = [];
+
+// for (let j = 0; j < (velikostPole[0]); j++) {
+//   let total = 0;
+
+//   let row = [];
+
+//   for (let k = 0; k < velikostPole[1]; k++) {
+//     total++;
+//     row.push(data[total].split(' '));
+//   }
+//   herniPole.push(row);
+// }
+// console.log(herniPole)
+
+
+// console.log(poleRyb(velikostPole[0], velikostPole[1]));
+
+
+
+  // Vytvoření dvojrozměrného pole s načtením všech hodno dle šířky a výšky
+  // Totéž pro cílové pozice ryb, kde by cyklus přeskošil a začal znova po dosažení velikosti šířky
+  // Následné posouvání hodnot dle šipky a dotazy k porovnání
+// for (let i = 0; i < pocetHer; i++) {
+  
+// }
--- script.js
+++ script.js
@@ -1,90 +1,104 @@
 let input = `1
-3 3
->  
- ∧ 
+3 6
+>>     
+ ∧    
+      
+  #   
+ ## 
    
-  #
- # `;
+ 3 8`;
+// Input není redundatní na jiná čísla
 
 // let data = input.toString();
-let data = input.split("\n");
+const data = input.split("\n");
 console.log(data)
 
 const pocetHer = Number(data[0]);
 
 const velikostPole = [data[1].charAt(0), data[1].charAt(2)];
+const vyskaPole = Number(velikostPole[0]);
 
+console.log("vyska pole: "  + vyskaPole)
 console.log("Rozměry pole: " + velikostPole);
 console.log("Prvni prvek v poli:" + data[2].charAt(0));
 
-let total = 3;
-
-let poleRyb = function(sirka, vyska) {
+let poleRyb = function(vyska, sirka) {
   let herniPole = [];
 
-  for (let j = 0; j < sirka; j++) {
+  for (let j = 0; j < vyska; j++) {
     let row = [];
 
-    for (let k = 0; k < vyska; k++) {
-      row.push(data[j + 2][k]);
-      // console.log("Tada s pridanim trojky: " + data[j + 3]);
+    for (let k = 0; k < sirka; k++) {
+      // row.push(data[j + 2][k]);
+      row.push(data[j + 2].charAt(k));
   }
   herniPole.push(row);
 }
   return herniPole;
 };
 
-console.log("Pole:")
+console.log("Pole ryb:")
 console.log(poleRyb(velikostPole[0], velikostPole[1]));
 
-//navazuje na "total hodnotu" z predchoziho cyklu
-let poleCilu = function(sirka, vyska) {
-  let herniPole = [];
+// poleCílů
 
-  for (let j = 0; j < sirka; j++) {
+let cilovePole = function(vyska, sirka) {
+  let cilovePole = [];
+
+  for (let j = 0; j < vyska; j++) {
     let row = [];
 
-    for (let k = 0; k < vyska; k++) {
-      row.push(data[j + 2][k]);
+    for (let k = 0; k < sirka; k++) {
+      row.push(data[j + 2 + vyskaPole].charAt(k));
+    }
+    cilovePole.push(row);
   }
-  herniPole.push(row);
-}
-  return herniPole;
+  return cilovePole;
 };
-// console.log(poleCilu(velikostPole[0], velikostPole[1]));
 
+console.log("Cílové pole:");
+console.log(cilovePole(velikostPole[0], velikostPole[1]))
+///
 
+// Zápis souřadnic, ideálně vložit do objektu/list, vyvolat abstraktní počítání se souřadnicemi z pole vysledků
+//Ryby filter
+let rybyFilter = poleRyb(velikostPole[0], velikostPole[1]);
+console.log("Pohled do jednoho prvku: " + rybyFilter[1][1])
+let count = 0;
 
+for(let y = 0; y < velikostPole[0]; y++) {
+  let radek = rybyFilter[y];
 
+  for(let x = 0; x < velikostPole[1]; x++) {
+    if (radek[x] == "∧" || radek[x] == "∨" || radek[x] == ">" || radek[x] == "<" || radek[x] == "x") {
 
+    console.log("Souřadnice ryby" + count + ": " + x + " " + y);
+    console.log(`Ryba${count} positionX: ${x} positionY: ${y} direction: ${radek[x]}`);
+      
+    // let souradniceRyb = [{jmeno: }]
+    count++;
+  }
+}
+}
+// Cíle filter
+let cileFilter = cilovePole(velikostPole[0], velikostPole[1]);
+let counter = 0;
 
+for(let y = 0; y < velikostPole[0]; y++) {
+  let radek = cileFilter[y];
 
-// let herniPole = [];
+  for(let x = 0; x < velikostPole[1]; x++) {
+    if (radek[x] == "#") {
+    console.log("Cílova pozice ryby: " + x + " " + y);
+    count++;
+  }
+}
+}
+// Maximální počet cyklů se dá vypočítat jako NSN
 
-// for (let j = 0; j < (velikostPole[0]); j++) {
-//   let total = 0;
+// 2 5 6 7 8 14 15
+// 1 2 8 11
 
-//   let row = [];
-
-//   for (let k = 0; k < velikostPole[1]; k++) {
-//     total++;
-//     row.push(data[total].split(' '));
-//   }
-//   herniPole.push(row);
-// }
-// console.log(herniPole)
-
-
-// console.log(poleRyb(velikostPole[0], velikostPole[1]));
-
-
-
-  // Vytvoření dvojrozměrného pole s načtením všech hodno dle šířky a výšky
-  // Totéž pro cílové pozice ryb, kde by cyklus přeskošil a začal znova po dosažení velikosti šířky
-  // Následné posouvání hodnot dle šipky a dotazy k porovnání
-// for (let i = 0; i < pocetHer; i++) {
-  
-// }
 
 
 
